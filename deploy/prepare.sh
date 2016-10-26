e5be6ada22e884c8aac2d910bb4cbee5d10e7618
Alexander Alekhin <alexander.alekhin@intel.com>
2016-10-26 13:58:48 +0300

    update 2016Q3

--- deploy/prepare.sh
+++ deploy/prepare.sh
@@ -25,18 +25,8 @@ mkdir -p /build/_repos
 cd /build/_repos
 /app/scripts/build_clone_repositories.sh /build
 
-mkdir -p /app/credentials
-if [ ! -f /app/credentials/id_rsa ]; then
-  ssh-keygen -q -C build@slave -f /app/credentials/id_rsa
-fi
-chmod 600 /app/credentials/id_rsa
-mkdir -p ${HOME}/.ssh
-cat > ${HOME}/.ssh/config << EOF
-Host *
-    StrictHostKeyChecking No
-    IdentityFile /app/credentials/id_rsa
-EOF
-chmod 644 ${HOME}/.ssh/config
+chmod 700 ${HOME}/.ssh
+chmod 600 ${HOME}/.ssh/*
 
 git config --global user.name "build"
 git config --global user.email "build@slave"
--- deploy/prepare_root.sh
+++ deploy/prepare_root.sh
@@ -4,6 +4,8 @@
   groupadd -r build -g $APP_GID
   useradd -u $APP_UID -r -g build -d /home/build -m -s /bin/bash -c "Build user" build
 }
+chown build:build /home/build
+[ ! -d /home/build/.ssh ] || chown -R build:build /home/build/.ssh
 
 mkdir -p /env
 chown -R build:build /env
@@ -17,6 +19,7 @@ chown -R build:build /build-3
 
 cp -rf /opt/arm-linux-gnueabihf/* /usr/arm-linux-gnueabihf/
 
+[ ! -d /opt/packages/ceres/build ] ||
 (
   cd /opt/packages/ceres/build
   make install || /bin/true
