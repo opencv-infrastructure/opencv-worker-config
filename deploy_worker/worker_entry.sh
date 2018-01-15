5dfc4823d372351b127bd6723d5be604611bccad
Alexander Alekhin <alexander.alekhin@intel.com>
2018-01-15 22:29:54 +0300

    opencl: support kernel 4.10+

--- deploy_worker/worker_entry.sh
+++ deploy_worker/worker_entry.sh
@@ -4,11 +4,15 @@ getent passwd build || {
   groupadd -r build -g $APP_GID
   useradd -u $APP_UID -r -g build -d /home/build -m -s /bin/bash -c "Build user" build
 }
-[ ! -e /dev/dri/controlD64 ] || {
+if [ -e /dev/dri/controlD64 ]; then
   VIDEO_GID=$(stat -c "%g" /dev/dri/controlD64)
   getent group video_build || groupadd -o video_build -g $VIDEO_GID
   usermod -aG video_build build
-}
+elif [ -e /dev/dri/renderD128 ]; then
+  VIDEO_GID=$(stat -c "%g" /dev/dri/renderD128)
+  getent group video_build || groupadd -o video_build -g $VIDEO_GID
+  usermod -aG video_build build
+fi
 
 chown build:build /home/build
 
