cfaa9b3134d309a8cbb3e955524da3d64f1796f8
Alexander Alekhin <alexander.alekhin@intel.com>
2018-02-06 18:48:25 +0300

    scripts: improve docker_cleanup.sh

--- scripts/docker_cleanup.sh
+++ scripts/docker_cleanup.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/bash -x
 docker stop $(docker ps --filter name="opencv_build_*" -q 2>/dev/null) 2>/dev/null
+sleep 2
 docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
+docker rm -v $(docker ps --filter name="opencv_build_*" --filter status=created -q 2>/dev/null) 2>/dev/null
 docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
