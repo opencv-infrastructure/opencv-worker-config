0da018ad68efb7aff2f21a1a074812827a009464
Alexander Alekhin <alexander.alekhin@intel.com>
2018-04-03 15:36:04 +0300

    +3.4

--- scripts/docker_cleanup.sh
+++ scripts/docker_cleanup.sh
@@ -4,3 +4,4 @@ sleep 2
 docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
 docker rm -v $(docker ps --filter name="opencv_build_*" --filter status=created -q 2>/dev/null) 2>/dev/null
 docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
+exit 0
