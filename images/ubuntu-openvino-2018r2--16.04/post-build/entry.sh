b4e5b2b32b3cfe802ae724632880e04a241386af
Alexander Alekhin <alexander.alekhin@intel.com>
2018-08-28 16:39:37 +0300

    +OpenVINO 2018R3

--- images/ubuntu-openvino-2018r2--16.04/post-build/entry.sh
+++ images/ubuntu-openvino-2018r2--16.04/post-build/entry.sh
@@ -7,7 +7,7 @@ DIR=${1?Missing workdir}
 IMAGE_NAME=${2?Missing image name}
 
 # Install steps (root)
-/opt/build-worker/intel-openvino/l_openvino_toolkit_p_2018.3.343/install.sh -s ${DIR}/silent.cfg
+/opt/build-worker/intel-openvino/l_openvino_toolkit_p_2018.2.300/install.sh -s ${DIR}/silent.cfg
 
 echo "Completed"
 exit 0
