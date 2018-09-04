d1d2422c44388e75d6de02aff617d44ed1deb7fb
Alexander Alekhin <alexander.alekhin@intel.com>
2018-09-04 12:51:50 +0300

    scripts: fix build_clone_repositories.sh
    "fixed"-branch builds don't use sub-directories 2.4/3.4 for sources

--- scripts/build_clone_repositories.sh
+++ scripts/build_clone_repositories.sh
@@ -5,28 +5,33 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 
-DIR=$(pwd)
-OPENCV_GIT_URL=${OPENCV_GIT_URL:-https://github.com/opencv}
-git clone --bare ${OPENCV_GIT_URL}/opencv.git
-git clone --bare ${OPENCV_GIT_URL}/opencv_extra.git
-git clone --bare ${OPENCV_GIT_URL}/opencv_contrib.git
+BUILD_DIR=${1?Missing build dir}
 
-pushd ${1}
+pushd ${BUILD_DIR}
+
+DIR=${BUILD_DIR}/_repos
+(
+  cd $DIR
+  OPENCV_GIT_URL=${OPENCV_GIT_URL:-https://github.com/opencv}
+  git clone --bare ${OPENCV_GIT_URL}/opencv.git || (cd opencv.git; git fetch origin)
+  git clone --bare ${OPENCV_GIT_URL}/opencv_extra.git || (cd opencv_extra.git; git fetch origin)
+  git clone --bare ${OPENCV_GIT_URL}/opencv_contrib.git || (cd opencv_contrib.git; git fetch origin)
+)
 
 set +e
 
-for d in ./*; do
-  if [[ -d "$d" ]] && [[ $d != "_repos" ]] && [[ $d != *logs ]] && [[ $d != *info ]]; then
-    echo $d
+for d in *; do
+  if [[ -d "$d" && $d != "_repos" && $d != "logs" && $d != "info" ]]; then
+    echo "======================================= $d "
     cd $d
-    if [[ $d == *precommit* ]] || [[ $d == *master* ]]; then
+    if [[ $d == precommit* || $d == master* || $d == 2_4* || $d == *3_4* ]]; then
       (
       git clone -s -n ${DIR}/opencv.git
       git clone -s -n ${DIR}/opencv_extra.git
       git clone -s -n ${DIR}/opencv_contrib.git
       )
     fi
-    if [[ $d == *precommit* ]] || [[ $d == *2_4* ]]; then
+    if [[ $d == precommit* ]]; then
       (
       echo "    +2.4"
       mkdir -p 2.4; cd 2.4;
@@ -34,8 +39,6 @@ for d in ./*; do
       git clone -s -n ${DIR}/opencv_extra.git
       #git clone -s -n ${DIR}/opencv_contrib.git
       )
-    fi
-    if [[ $d == *precommit* ]] || [[ $d == *3_4* ]]; then
       (
       echo "    +3.4"
       mkdir -p 3.4; cd 3.4;
