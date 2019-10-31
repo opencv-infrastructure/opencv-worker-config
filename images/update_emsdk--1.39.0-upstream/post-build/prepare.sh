b82c0d5d1c7c5985d5ea4cc63917e059511fe99d
Alexander Alekhin <alexander.alekhin@intel.com>
2019-10-31 19:02:49 +0300

    images: EMSDK_VERSION=1.39.0-upstream in docs-js, javascript, javascript-simd

---
+++ images/update_emsdk--1.39.0-upstream/post-build/prepare.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -e
+{ # force bash to read file completelly
+DIR=$1
+IMAGE_NAME=${2?Missing image name}
+
+. /app/deploy/env.sh
+. /app/images/$IMAGE_NAME/tools/env.sh
+
+export
+
+EMSDK_DIR=/opt/build-worker/emsdk-${EMSDK_VERSION}
+
+# fetch files from host cache
+mkdir -p /opt/build-containers-cache/emsdk-${EMSDK_VERSION}/
+mkdir -p ${EMSDK_DIR}
+rsync -axHAX /opt/build-containers-cache/emsdk-${EMSDK_VERSION}/ ${EMSDK_DIR}/
+
+pushd ${EMSDK_DIR}
+
+set -x
+git rev-parse HEAD || (rm -rf ./.git && git clone https://github.com/juj/emsdk.git .)
+git reset --hard || true
+git clean -f -d || true
+git fetch origin
+git checkout -B master origin/master
+git rev-parse HEAD
+
+# update git source cache back
+rsync -avxHAX --delete --checksum ${EMSDK_DIR}/ /opt/build-containers-cache/emsdk-${EMSDK_VERSION}/
+
+./emsdk list
+./emsdk install ${EMSDK_VERSION}
+./emsdk activate ${EMSDK_VERSION}
+source ./emsdk_env.sh
+export
+
+mkdir -p ./.home
+cp -rp ~/.em* ./.home/
+
+popd
+
+# update git source cache back
+rsync -axHAX --delete --checksum ${EMSDK_DIR}/ /opt/build-containers-cache/emsdk-${EMSDK_VERSION}/
+set +x
+
+echo "Done"
+exit 0
+}
---
+++ images/update_emsdk--1.39.0-upstream/tools/env.sh
@@ -0,0 +1,1 @@
+export EMSDK_VERSION=1.39.0-upstream
