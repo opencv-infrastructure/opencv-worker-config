0eb64f296abb0f60c90e519bab33355701d98ecb
Alexander Alekhin <alexander.alekhin@intel.com>
2020-02-06 14:15:42 +0300

    +OpenVINO 2020.1

---
+++ images/ubuntu-openvino-2020.1.0--16.04/post-build/entry.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+# Container started with these mounted volumes:
+# - /app
+# - /opt
+DIR=${1?Missing workdir}
+IMAGE_NAME=${2?Missing image name}
+
+# Install steps (root)
+/opt/build-worker/intel-openvino/l_openvino_toolkit_p_2020.1.023/install.sh -s ${DIR}/silent.cfg
+
+echo "Completed"
+exit 0
