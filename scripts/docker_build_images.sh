219923a0ea33bb56db5cb0d2b8d6a8f407820df3
Alexander Alekhin <alexander.a.alekhin@gmail.com>
2020-04-18 07:54:13 +0000

    docker-infra: update docker build image script
    - support caching for images with post-build steps
    - fix errors and Ctrl+C handling
    - always add proxy args (better usulization of docker cache)

--- scripts/docker_build_images.sh
+++ scripts/docker_build_images.sh
@@ -1,27 +1,39 @@
 #!/bin/bash -el
+{ # force bash to read file completelly
+set -e
+set -o pipefail
+set -u
+
+# bash 4.4+ is OK: https://stackoverflow.com/questions/7577052/bash-empty-array-expansion-with-set-u
+if ! echo $BASH_VERSION | awk -F. '{ if ($1 > 4 || ($1 == 4 && $2 >= 4)) { exit 0 } else {exit 1} }' ; then
+  set +u
+fi
+
+function handle_stop() {
+    echo ">>>>>>>> STOP <<<<<<<<"
+    STOP_SIGNAL=1
+}
+
+trap handle_stop SIGINT SIGTERM
+
 IMAGES_DIR=$1
 ROOT_DIR=`cd "$1"/.. && pwd`
 shift
 
 DOCKER_IMAGE_PREFIX=${DOCKER_IMAGE_PREFIX:-opencv-}
+DOCKER_CACHE_INFO_DIR=$(cd "${IMAGES_DIR}" && pwd)/.cache
 
-if type "proxy_setup" 2>/dev/null; then
+if type "proxy_setup" >/dev/null 2>&1; then
   proxy_setup
 fi
-if [ -n "$HTTP_PROXY" ]; then
-  DOCKER_BUILD_ARGS="$DOCKER_BUILD_ARGS --build-arg HTTP_PROXY=$HTTP_PROXY"
-  DOCKER_BUILD_ARGS="$DOCKER_BUILD_ARGS --build-arg http_proxy=$HTTP_PROXY"
-  DOCKER_RUN_ARGS="$DOCKER_RUN_ARGS --env HTTP_PROXY=$HTTP_PROXY"
-  DOCKER_RUN_ARGS="$DOCKER_RUN_ARGS --env http_proxy=$HTTP_PROXY"
-fi
-if [ -n "$HTTPS_PROXY" ]; then
-  DOCKER_BUILD_ARGS="$DOCKER_BUILD_ARGS --build-arg HTTPS_PROXY=$HTTPS_PROXY"
-  DOCKER_BUILD_ARGS="$DOCKER_BUILD_ARGS --build-arg https_proxy=$HTTPS_PROXY"
-  DOCKER_RUN_ARGS="$DOCKER_RUN_ARGS --env HTTPS_PROXY=$HTTPS_PROXY"
-  DOCKER_RUN_ARGS="$DOCKER_RUN_ARGS --env https_proxy=$HTTPS_PROXY"
-fi
+HTTP_PROXY=${HTTP_PROXY:-${http_proxy:-}}
+DOCKER_BUILD_ARGS="${DOCKER_BUILD_ARGS:-} --build-arg HTTP_PROXY=$HTTP_PROXY --build-arg http_proxy=$HTTP_PROXY"
+HTTPS_PROXY=${HTTPS_PROXY:-${https_proxy:-}}
+DOCKER_BUILD_ARGS="$DOCKER_BUILD_ARGS --build-arg HTTPS_PROXY=$HTTPS_PROXY --build-arg https_proxy=$HTTPS_PROXY"
+NO_PROXY=${NO_PROXY:-${no_proxy:-}}
+DOCKER_BUILD_ARGS="$DOCKER_BUILD_ARGS --build-arg NO_PROXY=$NO_PROXY --build-arg no_proxy=$NO_PROXY"
 
-if [ -n "$NOCACHE" ]; then
+if [ -n "${NOCACHE:-}" ]; then
   DOCKER_BUILD_ARGS="--no-cache $DOCKER_BUILD_ARGS"
 fi
 # Doesn't work, because intermediate images are not pushed into registry
@@ -42,7 +54,7 @@ exec 9>&1
 execute()
 {
   echo $BOLD$YELLOW$*$RESET 1>&9
-  [[ "$DRY_RUN" > 0 ]] || {
+  [[ "${DRY_RUN:-}" > 0 ]] || {
     /usr/bin/time $* || {
       RC=$?; echo "  "$ERROR_STYLE"result=$RC"$RESET 1>&9; return $RC
     }
@@ -108,36 +120,85 @@ build_image()
     echo "Processing image: $docker_image ($image)"
     cd $IMAGES_DIR/$image/docker
     if [ -d ../post-build ]; then
-      execute docker build $DOCKER_BUILD_ARGS -t staging-$docker_image .
-      echo "Docker build completed at $(date '+%Y-%m-%d %H:%M:%S'): $docker_image ($image)"
+      HASH_POST_BUILD=$(tar -cf - -C ../post-build . | sha256sum -b | cut -d ' ' -f 1)
+      echo "${docker_image}: Post-build scripts hash: ${HASH_POST_BUILD}"
+      # TODO: other dirs, like /tools with env versions settings
+
+      IMAGE_STAGING=staging-$docker_image
+      execute docker build $DOCKER_BUILD_ARGS -t ${IMAGE_STAGING} . ||
+      {
+        echo "==============================================================================="
+        echo "*  FATAL: Can't build staging image: $docker_image ($image)"
+        echo "==============================================================================="
+        exit 1
+      }
+      echo "Docker staging build completed at $(date '+%Y-%m-%d %H:%M:%S'): $docker_image ($image)"
+      HASH_DOCKER_IMAGE_STAGING=$(docker inspect --type=image --format='{{index .Id}}' ${IMAGE_STAGING} | cut -d ':' -f 2)
       (
-      set +e
+      set -e
+      WORKDIR=/app/images/$image/post-build
+      DOCKER_ARGS=(
+-v ${ROOT_DIR}:/app:ro
+-v /opt/build-worker:/opt/build-worker:rw
+-v /opt/build-containers-cache:/opt/build-containers-cache:rw
+-v /opt/android:/opt/android:ro
+${DOCKER_RUN_ARGS:-}
+${IMAGE_STAGING}
+"${WORKDIR}/entry.sh" "${WORKDIR}" "$image"
+)
+      HASH_DOCKER_ARGS=$(echo "${DOCKER_ARGS[@]}" | sed -e s~${ROOT_DIR}~ROOT_DIR~g | sha256sum | cut -d ' ' -f 1)
+
+      CACHE_FILE_NAME="${DOCKER_CACHE_INFO_DIR}/${image}.info"
+      if [[ ! -n "${NOCACHE:-}" && -f "${CACHE_FILE_NAME}" ]]; then
+        echo "${docker_image}: Checking cache..."
+        CACHE_IMAGE_ID=($(cat "${CACHE_FILE_NAME}"))
+        if [[ "${CACHE_IMAGE_ID[1]}" == "${HASH_POST_BUILD}" &&
+              "${CACHE_IMAGE_ID[2]}" == "${HASH_DOCKER_IMAGE_STAGING}" &&
+              "${CACHE_IMAGE_ID[3]}" == "${HASH_DOCKER_ARGS}" ]]; then
+          echo "${docker_image}: Image cache candidate: ${CACHE_IMAGE_ID}"
+          if docker image inspect "${CACHE_IMAGE_ID}" >/dev/null 2>&1 ; then
+            (
+              execute docker tag ${CACHE_IMAGE_ID} $docker_image || exit 1
+              docker rmi --no-prune ${IMAGE_STAGING} || /bin/true
+            ) && exit 0 || /bin/true
+          else
+            echo "${docker_image}:     Image cache candidate is not available"
+          fi
+        else
+            echo "${docker_image}: Cache miss"
+        fi
+      fi
+
       CONTAINER_NAME="preparing-$docker_image"
       CONTAINER_NAME=${CONTAINER_NAME//[^A-Za-z0-9._-]/_}
-      WORKDIR=/app/images/$image/post-build
-      docker rm ${CONTAINER_NAME} || /bin/true
-      execute docker run -it --name ${CONTAINER_NAME} \
-          -v ${ROOT_DIR}:/app:ro \
-          -v /opt/build-worker:/opt/build-worker:rw \
-          -v /opt/build-containers-cache:/opt/build-containers-cache:rw \
-          -v /opt/android:/opt/android:ro \
-          ${DOCKER_RUN_ARGS} \
-          staging-$docker_image \
-          ${WORKDIR}/entry.sh "${WORKDIR}" "$image"
-      STATUS=`docker inspect ${CONTAINER_NAME} --format='{{.State.ExitCode}}'`
+      docker inspect ${CONTAINER_NAME} >/dev/null 2>&1 && docker rm ${CONTAINER_NAME} || /bin/true
+      execute docker run -it --name ${CONTAINER_NAME} ${DOCKER_ARGS[@]} || exit 1
+
+      STATUS=`docker inspect ${CONTAINER_NAME} --format='{{.State.ExitCode}}' || echo 1`
       if [[ $STATUS != 0 ]]; then
-         echo "==============================================================================="
-         echo "*  FATAL: Can't build image: $docker_image ($image)"
-         echo "==============================================================================="
-         exit 1
+        echo "==============================================================================="
+        echo "*  FATAL: Post-build step failed for image: $docker_image ($image)"
+        echo "==============================================================================="
+        exit 1
       fi
-      execute docker commit ${CONTAINER_NAME} $docker_image
+      execute docker commit ${CONTAINER_NAME} $docker_image || exit 1
       docker rm ${CONTAINER_NAME} || /bin/true
-      docker rmi --no-prune staging-$docker_image || /bin/true
+      docker rmi --no-prune ${IMAGE_STAGING} || /bin/true
+
+      HASH_DOCKER_IMAGE=$(docker inspect --type=image --format='{{index .Id}}' $docker_image | cut -d ':' -f 2)
+      mkdir -p ${DOCKER_CACHE_INFO_DIR} || /bin/true
+      echo "${HASH_DOCKER_IMAGE} ${HASH_POST_BUILD} ${HASH_DOCKER_IMAGE_STAGING} ${HASH_DOCKER_ARGS}" > "${CACHE_FILE_NAME}" ||
+          echo "WARNING: Can't update image cache info"
       ) || exit 1
       echo "Done at $(date '+%Y-%m-%d %H:%M:%S'): $docker_image ($image)"
     else
-      time docker build $DOCKER_BUILD_ARGS -t $docker_image .
+      execute docker build $DOCKER_BUILD_ARGS -t $docker_image . ||
+      {
+        echo "==============================================================================="
+        echo "*  FATAL: Can't build image: $docker_image ($image)"
+        echo "==============================================================================="
+        exit 1
+      }
       echo "Done at $(date '+%Y-%m-%d %H:%M:%S'): $docker_image ($image)"
     fi
   ) ||
@@ -150,7 +211,10 @@ build_image()
 if [ "$#" -ge 1 ]; then
   echo "Build images: $*"
   for image in $*; do
-    build_image $image
+    build_image $image || /bin/true
+    if [ -n "${STOP_SIGNAL:-}" ]; then
+      break
+    fi
   done
 else
   echo "Build images from: $IMAGES_DIR"
@@ -168,9 +232,20 @@ else
         build_image "$BASENAME" || /bin/true
       fi
     fi
-  done
-  for image in ${FAILED_IMAGES[@]}; do
-    echo $ERROR_STYLE"Failed image: $image"$RESET
+    if [ -n "${STOP_SIGNAL:-}" ]; then
+      break
+    fi
   done
 fi
+echo $RESET"==============================================================================="
+for image in ${FAILED_IMAGES[@]}; do
+  echo $ERROR_STYLE"Failed image: $image"$RESET
+done
+if [ ${#FAILED_IMAGES[@]} -gt 0 ]; then
+  echo "FAILED"
+  exit 1
+fi
 echo "Done: $(date)"
+
+exit 0
+} # force bash to read file completelly
