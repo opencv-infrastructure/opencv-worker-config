7a03f3c0c86ffe0befad976494351019d6498e44
Alexander Alekhin <alexander.a.alekhin@gmail.com>
2021-10-27 18:08:48 +0000

    scripts: handle both video/render groups

--- deploy_worker/worker_entry.sh
+++ deploy_worker/worker_entry.sh
@@ -8,14 +8,20 @@ getent passwd build || {
   groupadd -r build -g $APP_GID
   useradd -u $APP_UID -r -g build -d /home/build -m -s /bin/bash -c "Build user" build
 }
-if [ -e /dev/dri/controlD64 ]; then
+if [ -e /dev/dri/card0 ]; then
+  VIDEO_GID=$(stat -c "%g" /dev/dri/card0)
+  getent group video_build || groupadd -o video_build -g $VIDEO_GID
+  usermod -aG video_build build
+elif [ -e /dev/dri/controlD64 ]; then
   VIDEO_GID=$(stat -c "%g" /dev/dri/controlD64)
   getent group video_build || groupadd -o video_build -g $VIDEO_GID
   usermod -aG video_build build
-elif [ -e /dev/dri/renderD128 ]; then
-  VIDEO_GID=$(stat -c "%g" /dev/dri/renderD128)
-  getent group video_build || groupadd -o video_build -g $VIDEO_GID
-  usermod -aG video_build build
+fi
+
+if [ -e /dev/dri/renderD128 ]; then
+  RENDER_GID=$(stat -c "%g" /dev/dri/renderD128)
+  getent group render_build || groupadd -o render_build -g $RENDER_GID
+  usermod -aG render_build build
 fi
 
 chown build:build /home/build
