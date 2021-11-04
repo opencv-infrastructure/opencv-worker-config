6873e01e35e022971e3346e32618250f03672c25
Alexander Alekhin <alexander.a.alekhin@gmail.com>
2021-11-04 13:11:01 +0000

    +images: oneapi-2021.4.0{,-clang,-icc}, onevpl-2021.6.0
    based on opencv-ubuntu:20.04

--- scripts/container_options.sh
+++ scripts/container_options.sh
@@ -1,25 +1,50 @@
 get_image_script()
 {
   local image=$1
-  if [[ -n "$image" ]]; then
-    local script_name="/app/scripts/container_options-$image.sh"
+  local suffix=$2
+  if [[ -n "$suffix" ]]; then
+    suffix="-${suffix}"
+  fi
+  if [[ "${image}" == "${BUILD_IMAGE}" ]]; then
+    local script_name="/app/images/${BUILD_IMAGE}/container_options$suffix.sh"
     if [ -f "${script_name}" ]; then
       echo "${script_name}"
+      return 0
     fi
-  else
+  fi
+  local script_name="/app/scripts/container_options-$image$suffix.sh"
+  if [ -f "${script_name}" ]; then
+    echo "${script_name}"
     return 0
   fi
+  return 0
 }
 
-script_name=$(get_image_script $BUILD_IMAGE-pre)
+script_name=$(get_image_script "worker_init")
 if [ -n "${script_name}" ]; then
   . "${script_name}" "$@"
 fi
-script_name=$(get_image_script all)
+script_name=$(get_image_script "$BUILD_IMAGE" "worker_pre")
 if [ -n "${script_name}" ]; then
   . "${script_name}" "$@"
 fi
-script_name=$(get_image_script $BUILD_IMAGE)
+script_name=$(get_image_script "$BUILD_IMAGE" "pre")
+if [ -n "${script_name}" ]; then
+  . "${script_name}" "$@"
+fi
+script_name=$(get_image_script "all" "")
+if [ -n "${script_name}" ]; then
+  . "${script_name}" "$@"
+fi
+script_name=$(get_image_script "$BUILD_IMAGE" "")
+if [ -n "${script_name}" ]; then
+  . "${script_name}" "$@"
+fi
+script_name=$(get_image_script "$BUILD_IMAGE" "worker_post")
+if [ -n "${script_name}" ]; then
+  . "${script_name}" "$@"
+fi
+script_name=$(get_image_script "worker_finalize")
 if [ -n "${script_name}" ]; then
   . "${script_name}" "$@"
 fi
